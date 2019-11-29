ec69ac1072a7807478633b3c1392398d57d5389e
yoelcortes <yoelcortes@gmail.com>
2019-11-29 01:45:45 -0600

    update

--- chemicals/base/thermo_model_handle.py
+++ chemicals/base/thermo_model_handle.py
@@ -19,6 +19,18 @@ def find_constant_model(models, T, P):
         if isa(model, CTM) and model.indomain(T, P): return model
             
 
+def get_not_a(obj, cls):
+	isinstance = isa
+	if isa(obj, cls):
+		return None
+	elif hasattr(obj, '__iter__'):
+		for i in object:
+			if not isa(i, cls):
+				return i
+	else:
+		return obj
+
+
 # %% Handles
 
 @functor_lookalike
@@ -37,6 +49,12 @@ class ThermoModelHandle:
     def __getitem__(self, index):
         return self.models[index]
     
+	def __setitem__(self, index, model):
+		not_a_model = get_not_a(model, ThermoHandle)
+		assert not_a_model is None, ("a 'ThermoModelHandle' may only contain "
+									f"'ThermoModel' objects, not a '{not_a_model}'")
+		self.models[index] = model
+	
     def __iter__(self):
         return iter(self.models)
     
