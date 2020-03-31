7096d032d5c78e64af950bbd6d1c89558dff0e94
yoelcortes <yoelcortes@gmail.com>
2020-03-31 14:01:35 -0500

    update

--- build/lib/thermosteam/utils/registry.py
+++ build/lib/thermosteam/utils/registry.py
@@ -4,6 +4,7 @@ Created on Wed Dec  5 16:47:33 2018
 
 @author: Yoel Cortes-Pena
 """
+from warnings import warn
 
 __all__ = ('Registry',)
 
@@ -11,12 +12,6 @@ class Registry:
     
     def search(self, ID):
         return self.__dict__.get(ID)
-    
-    def __setattr__(self, ID, obj):
-        self[ID] = obj
-    
-    def __iter__(self):
-        return iter(self.__dict__.values())
     
     def __getitem__(self, ID):
         return self.__dict__[ID]
@@ -29,7 +24,26 @@ class Registry:
         assert all([word.isalnum() for word in ID_words if word]), (
                 'ID may only contain letters, numbers, and/or underscores; '
                 'no special characters or spaces')
-        self.__dict__[ID] = obj
+        dct = self.__dict__
+        if ID in dct and dct[ID] is not obj:
+            warn(RuntimeWarning(f"ID, '{ID}', already registered"))
+        else:
+            dct[ID] = obj
+    
+    def __setattr__(self, ID, obj):
+        self[ID] = obj
+    
+    def discard(self, ID):
+        dct = self.__dict__
+        try: dct[ID].disconnect()
+        except AttributeError: pass
+        del dct[ID]
+    
+    def __contains__(self, ID):
+        return ID in self.__dict__
+    
+    def __iter__(self):
+        return iter(self.__dict__.values())
     
     def __repr__(self):
         if self.__dict__:
