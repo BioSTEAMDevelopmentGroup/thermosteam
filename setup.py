cae227826aab4b4eca2b2d4e8c00aade4437fe14
Yoel <yoelcortes@gmail.com>
2020-09-07 03:55:50 -0500

    bump up version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     name='thermosteam',
     packages=['thermosteam'],
     license='MIT',
-    version='0.20.15',
+    version='0.20.16',
     description="BioSTEAM's Premier Thermodynamic Engine",
     long_description=open('README.rst').read(),
     author='Yoel Cortes-Pena',
