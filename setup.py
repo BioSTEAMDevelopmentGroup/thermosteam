5e382244621300c2b1c2aed3a6e94152d725aef5
Yoel <yoelcortes@gmail.com>
2020-11-27 20:09:22 -0600

    make updates for PyPI

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     name='thermosteam',
     packages=['thermosteam'],
     license='MIT',
-    version='0.22.4',
+    version='0.22.5',
     description="BioSTEAM's Premier Thermodynamic Engine",
     long_description=open('README.rst').read(),
     author='Yoel Cortes-Pena',
