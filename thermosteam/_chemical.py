37775e0bc61264965984fd0ca27d0af62e00e1b7
Yalin <zoe.yalin.li@gmail.com>
2022-02-18 10:41:03 -0600

    minor bug fix for Chemical with shorter ID

--- thermosteam/_chemical.py
+++ thermosteam/_chemical.py
@@ -503,11 +503,12 @@ class Chemical:
                 warn('cached chemical returned; additional parameters disregarded')
             return chemical_cache[ID]
         ID = ID.strip()
-        if not phase and ID[-2] == ',': 
-            phase = ID[-1]
-            if phase not in valid_phases:
-                raise ValueError(f'invalid phase {repr(phase)} encountered while parsing ID')
-            ID = ID[:-2]
+        if not phase and len(ID)>=2:
+            if ID[-2] == ',': 
+                phase = ID[-1]
+                if phase not in valid_phases:
+                    raise ValueError(f'invalid phase {repr(phase)} encountered while parsing ID')
+                ID = ID[:-2]
         search_ID = search_ID or ID
         if not eos: eos = PR
         if search_db:
