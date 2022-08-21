9ff68a785a939fc72bf1fe3406c536d3f84be293
Yoel <yoelcortes@gmail.com>
2022-08-21 11:23:44 -0500

    test sphinx redirects

--- docs/conf.py
+++ docs/conf.py
@@ -3,3 +3,10 @@
 Empty because docs are built through BioSTEAM (not thermosteam).
 """
 
+extensions = [
+    'sphinx_reredirects',
+]
+
+redirects = {
+    "/*": "https://biosteam.readthedocs.io",
+}
