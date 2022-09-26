ffcbd23e956faeeaa6c660b9fdb10e31fd0d274c
Yoel <yoelcortes@gmail.com>
2022-09-25 22:27:22 -0500

    add test module for chemicals

---
+++ tests/test_chemicals.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# BioSTEAM: The Biorefinery Simulation and Techno-Economic Analysis Modules
+# Copyright (C) 2020-2022, Yoel Cortes-Pena <yoelcortes@gmail.com>
+# 
+# This module is under the UIUC open-source license. See 
+# github.com/BioSTEAMDevelopmentGroup/biosteam/blob/master/LICENSE.txt
+# for license details.
+"""
+"""
+import pytest
+import thermosteam as tmo
+from numpy.testing import assert_allclose
+
+def test_aliases():
+    chemicals = tmo.Chemicals(['Water', 'Ethanol', 'Methanol'], cache=True)
+    chemicals.compile()
+    chemicals.define_group('alcohols', ['Water', 'Ethanol'])
+    assert chemicals.get_aliases('H2O') == ['7732-18-5', 'Water', 'water', 'oxidane', 'H2O']
+    
+    
