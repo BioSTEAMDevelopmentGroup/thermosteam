54f4b58ca394977ad0bada10f839b51d0be1feb9
Yoel <yoelcortes@gmail.com>
2025-03-28 12:54:05 -0500

    filter empty nodes

--- thermosteam/nodes.py
+++ thermosteam/nodes.py
@@ -25,10 +25,12 @@ class EquationNode:
         self.name = name
         
     def set_equations(self, *nodes):
+        filtered_nodes = []
         for i in nodes:
+            if i is None: continue
             if not isinstance(i, VariableNode):
                 raise ValueError('equation nodes can only be connected to variable nodes')
-        self.nodes = nodes
+        self.nodes = tuple(filtered_nodes)
         
     def get_connections(self):
         return [VariableEquationConnection(self, i) for i in self.nodes]
@@ -41,12 +43,15 @@ class VariableNode:
     __slots__ = ('name', 'value', 'nodes',)
     
     def __init__(self, name, value, *nodes):
+        filtered_nodes = []
         for i in nodes:
+            if i is None: continue
             if not isinstance(i, EquationNode):
                 raise ValueError('variable nodes can only be connected to equation nodes')
+            filtered_nodes.append(i)
         self.name = name
         self.value = value
-        self.nodes = nodes
+        self.nodes = tuple(filtered_nodes)
         
     def get_connections(self):
         return [VariableEquationConnection(self, i) for i in self.nodes]
